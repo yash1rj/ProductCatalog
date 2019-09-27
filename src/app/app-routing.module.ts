a02ea12a0429dff87f298a5e28dc2c8f75ba775e
Yash Raj <yashrj130@gmail.com>
2019-09-27 18:07:58 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProdAddComponent } from './prod-add/prod-add.component';
+import { ProdGetComponent } from './prod-get/prod-get.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: ProdGetComponent },
+  { path: 'product/create', component: ProdAddComponent },
+  { path: '**', redirectTo: 'products', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
