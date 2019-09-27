62cb10db4a1aa05518621bb54e6d10cba5185fee
Yash Raj <yashrj130@gmail.com>
2019-09-27 18:09:43 +0530

    product get page controller

---
+++ src/app/prod-get/prod-get.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prod-get',
+  templateUrl: './prod-get.component.html',
+  styleUrls: ['./prod-get.component.css']
+})
+export class ProdGetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
