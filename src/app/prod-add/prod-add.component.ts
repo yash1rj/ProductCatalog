134dabca1c3fe92ad98fff6e4695669339d3232d
Yash Raj <yashrj130@gmail.com>
2019-09-30 17:38:23 +0530

    prod-add component

--- src/app/prod-add/prod-add.component.ts
+++ src/app/prod-add/prod-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { ProductsService } from '../products.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-prod-add',
@@ -11,7 +13,7 @@ export class ProdAddComponent implements OnInit {
   prodAddForm: FormGroup;
   prodBody: object;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private ps: ProductsService, private router: Router) {
     this.prodAddForm = this.fb.group({
       prodname: ['', [Validators.required, Validators.minLength(3)]],
       proddesc: ['', [Validators.required, Validators.minLength(3)]],
@@ -24,6 +26,17 @@ export class ProdAddComponent implements OnInit {
 
   onSubmit() {
     this.prodBody = this.prodAddForm.value;
-    console.log(this.prodBody);
+    // console.log(this.prodBody);
+    this.ps.addProduct(this.prodBody).subscribe(
+      (data)  => {
+        console.log("POST Request is successful ", data);
+        this.prodAddForm.reset();
+        this.router.navigate(['products']);
+        // this.registerservice.regSuccess(); 
+      },
+      (error)  => {
+        console.log("Error", error);
+      }
+    );
   }
 }
