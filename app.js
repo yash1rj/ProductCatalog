97853ecbda683360d89ff35c9eb70edb8c77d4e9
yash1rj <yashrj130@gmail.com>
2019-10-02 11:02:35 +0530

    for deployment

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
+const path = require('path');
 
 var port = process.env.PORT || 3000;
 
 var mongoose = require('mongoose');
 
-var Product = require('./restapi/model/productModel');
+var Product = require('./productCatalog-API/restapi/model/productModel');
 
 var bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/catalogger', { useNewUrlParser: true }).then(
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/catalogger', { useNewUrlParser: true }).then(
     () => {console.log('Database is connected') },
     err => { console.log('Can not connect to the database'+ err)}
 );
@@ -21,9 +22,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-var routes = require('./restapi/routes/productRoutes');
+var routes = require('./productCatalog-API/restapi/routes/productRoutes');
 
 routes(app);
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/product-catalog'));
+
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/product-catalog/index.html'));
+});
 
 app.use((req, res) => {
     res.status(404).send({url: req.originalUrl + ' not found'})
